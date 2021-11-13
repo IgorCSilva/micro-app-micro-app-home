6215fc2cd507eff90e9345abfb126ce863149ef3
Igor Carneiro <igor.carneiro.silva13@gmail.com>
2021-11-13 18:39:29 -0300

    Alterando title

--- lib/app/pages/home_page.dart
+++ lib/app/pages/home_page.dart
@@ -13,7 +13,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Homene'),
+        title: const Text('Homenes'),
       ),
     );
   }
